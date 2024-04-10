b745024f2a780fce5c167ac00fe53dcf95cffa53
Geovanne <geovamelo4431@gmail.com>
2024-04-09 23:51:49 -0300

    in progress - comecando

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
-      <h1>Menu</h1>
+      <Header />
+      <Sidebar />
       <main>
         <Outlet />
       </main>
