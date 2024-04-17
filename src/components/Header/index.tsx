0a49a7e06f972c2fb1f269c7c392e1699cf88523
Geovanne-Santos <geovamelo4431@gmail.com>
2024-04-16 21:45:52 -0300

    feat - nasod

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
 export function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className="w-screen flex justify-between py-8 px-4 border-b-4 border-white-500">
+      <div className="flex items-center gap-10">
+        <span>icone foda</span>
+        <h1 className="text-2xl font-bold">FATEKINHO</h1>
+      </div>  
+
+      <div className="flex">
+        <button>Cadastrar</button>
+        <button>Entrar</button>
+      </div>
+    </header>
+  ) 
 }
