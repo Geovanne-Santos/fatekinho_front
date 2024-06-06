c5853cda04d27d73387ac6678e8fca72ed0c1f11
Geovanne-Santos <geovamelo4431@gmail.com>
2024-06-06 01:03:44 -0300

    feat: starting login screen

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,13 @@
 export function Login() {
   return (
-    <section className="w-full h-screen">
-      <p>asa</p>
+    <section className="flex justify-center items-center w-full h-full bg-coins">
+      <div className="flex items-center justify-center w-2/4">
+        <h1>Login</h1>
+        <div>
+          <label htmlFor="">Email Cadastrado *:</label>
+          <input type="text" />
+        </div>
+      </div>
     </section>
   );
 }
