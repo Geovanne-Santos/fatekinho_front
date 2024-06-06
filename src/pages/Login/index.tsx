aabef198c5d240bb99ede086a6b9a4f25bb226d9
Geovanne-Santos <geovamelo4431@gmail.com>
2024-06-06 20:18:00 -0300

    feat: working..

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,9 @@
+import Car from "../../assets/car.png";
+
 export function Login() {
   return (
     <section className="flex justify-center items-center w-full h-full bg-coins">
-      <div className="flex items-center justify-center w-2/4">
+      <div className="flex items-center justify-center w-2/5 h-2/5 bg-[#000000] rounded-3xl" style={{ backgroundImage: `url(${Car})`}}>
         <h1>Login</h1>
         <div>
           <label htmlFor="">Email Cadastrado *:</label>
