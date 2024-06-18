6e30ca1384466c4b04da1f5964560e72ad165cab
GabrielViana14 <gabrielantonio1423@gmail.com>
2024-06-18 12:24:37 -0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/components/Sidebar/index.tsx
    #	src/router.tsx

--- src/pages/Roleta/index.tsx
+++ src/pages/Roleta/index.tsx
@@ -66,89 +66,92 @@ export function Roleta() {
     }, []);
     return (
         <>
-            <div className="menu-lateral mt-16">
-                <div className="container-user">
-                    <p id="user_name">Moedas</p>
-                    <input type="number" name="" id="user_saldo" value={0} readOnly={true}></input>
-                </div>
-                <div className="input-container">
-                    <input type="number" name="int_moedas" id="bet_valor" value={0} readOnly={true}></input>
-                    <button className="btn_add um">+1</button>
-                    <button className="btn_add dez">+10</button>
-                    <button className="btn_add cem">+100</button>
-                    <button className="btn_add all">+All</button>
-                </div>
-                <h2>Selecionar cor:</h2>
-                <div className="btn_container">
-                    <button className="aposta amarelo">1.5X</button>
-                    <button className="aposta vermelho">1.5X</button>
-                    <button className="aposta coringa">6X</button>
-                </div>
-                <h2>Selecionar numero:</h2>
-                <div className="container_colunas">
-                    <div className="column">
-                        <button className="btn_impar">1</button>
-                        <button className="btn_par">4</button>
-                        <button className="btn_impar">7</button>
-                        <button className="btn_par">10</button>
-                        <button className="btn_impar">13</button>
-                        <button className="btn_par">16</button>
-                        <button className="btn_impar">19</button>
-                        <button className="btn_par">22</button>
-                        <button className="btn_impar">25</button>
-                        <button className="btn_par">28</button>
-                        <button className="btn_impar">31</button>
-                        <button className="btn_par">34</button>
+            <div className="corpo">
+                <div className="menu-lateral mt-16">
+                    <div className="container-user">
+                        <p id="user_name">Moedas</p>
+                        <input type="number" name="" id="user_saldo" value={0} readOnly={true}></input>
                     </div>
+                    <div className="input-container-r">
+                        <input type="number" name="int_moedas" id="bet_valor" value={0} readOnly={true}></input>
+                        <button className="btn_add um">+1</button>
+                        <button className="btn_add dez">+10</button>
+                        <button className="btn_add cem">+100</button>
+                        <button className="btn_add all">+All</button>
+                    </div>
+                    <h2>Selecionar cor:</h2>
+                    <div className="btn_container">
+                        <button className="aposta amarelo">1.5X</button>
+                        <button className="aposta vermelho">1.5X</button>
+                        <button className="aposta coringa">6X</button>
+                    </div>
+                    <h2>Selecionar numero:</h2>
+                    <div className="container_colunas">
+                        <div className="column">
+                            <button className="btn_impar">1</button>
+                            <button className="btn_par">4</button>
+                            <button className="btn_impar">7</button>
+                            <button className="btn_par">10</button>
+                            <button className="btn_impar">13</button>
+                            <button className="btn_par">16</button>
+                            <button className="btn_impar">19</button>
+                            <button className="btn_par">22</button>
+                            <button className="btn_impar">25</button>
+                            <button className="btn_par">28</button>
+                            <button className="btn_impar">31</button>
+                            <button className="btn_par">34</button>
+                        </div>
 
-                    <div className="column">
-                        <button className="btn_par">2</button>
-                        <button className="btn_impar">5</button>
-                        <button className="btn_par">8</button>
-                        <button className="btn_impar">11</button>
-                        <button className="btn_par">14</button>
-                        <button className="btn_impar">17</button>
-                        <button className="btn_par">20</button>
-                        <button className="btn_impar">23</button>
-                        <button className="btn_par">26</button>
-                        <button className="btn_impar">29</button>
-                        <button className="btn_par">32</button>
-                        <button className="btn_impar">35</button>
-                    </div>
+                        <div className="column">
+                            <button className="btn_par">2</button>
+                            <button className="btn_impar">5</button>
+                            <button className="btn_par">8</button>
+                            <button className="btn_impar">11</button>
+                            <button className="btn_par">14</button>
+                            <button className="btn_impar">17</button>
+                            <button className="btn_par">20</button>
+                            <button className="btn_impar">23</button>
+                            <button className="btn_par">26</button>
+                            <button className="btn_impar">29</button>
+                            <button className="btn_par">32</button>
+                            <button className="btn_impar">35</button>
+                        </div>
 
-                    <div className="column">
-                        <button className="btn_impar">3</button>
-                        <button className="btn_par">6</button>
-                        <button className="btn_impar">9</button>
-                        <button className="btn_par">12</button>
-                        <button className="btn_impar">15</button>
-                        <button className="btn_par">18</button>
-                        <button className="btn_impar">21</button>
-                        <button className="btn_par">24</button>
-                        <button className="btn_impar">27</button>
-                        <button className="btn_par">30</button>
-                        <button className="btn_impar">33</button>
-                        <button className="btn_par">36</button>
+                        <div className="column">
+                            <button className="btn_impar">3</button>
+                            <button className="btn_par">6</button>
+                            <button className="btn_impar">9</button>
+                            <button className="btn_par">12</button>
+                            <button className="btn_impar">15</button>
+                            <button className="btn_par">18</button>
+                            <button className="btn_impar">21</button>
+                            <button className="btn_par">24</button>
+                            <button className="btn_impar">27</button>
+                            <button className="btn_par">30</button>
+                            <button className="btn_impar">33</button>
+                            <button className="btn_par">36</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="tela-jogo mt-16">
-                <div className="screen">
-                    <div id="resultado">Girando...</div>
-                    <div className='carrosel'>
-                        <div className='seta'></div>
-                        <div className='container'></div>
+                <div className="tela-jogo">
+                    <div className="screen1">
+                        <div id="resultado">Girando...</div>
+                        <div className='carrosel'>
+                            <div className='seta'></div>
+                            <div className='container'></div>
+                        </div>
                     </div>
-                </div>
-                <div className="menu-inferior">
-                    <h4>Giros anteriores</h4>
-                    <div className="container-anteriores">
-                        <div className="carrosel-anteriores">
+                    <div className="menu-inferior">
+                        <h4>Giros anteriores</h4>
+                        <div className="container-anteriores">
+                            <div className="carrosel-anteriores">
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </>
     )
 }
