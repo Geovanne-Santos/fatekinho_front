3d889f51c1960458c1890a86aa251248613bf400
Geovanne-Meloni <geovanne.meloni1@gmail.com>
2024-06-18 17:04:38 -0300

    feat: Saldo atualizando

--- src/api/services/login.ts
+++ src/api/services/login.ts
@@ -2,6 +2,6 @@ import { api } from "./axios"
 
 export const login = async (body: any) => {
     const response = await api.post("/usuario/login", body);
-
+    console.log(response.data)
     return response.data;
 } 
--- src/features/auth/fatecoins.ts
+++ src/features/auth/fatecoins.ts
@@ -2,24 +2,36 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../store';
 
 interface FatecoinState {
-    coins: number;
+    coins: number | null; // Permitir que coins seja null
 }
 
 const initialState: FatecoinState = {
-    coins: localStorage.getItem("player-money") ? JSON.parse(localStorage.getItem("user") || "").money : null,
+    coins: (() => {
+        const storedMoney = localStorage.getItem("player-money");
+        if (storedMoney) {
+            try {
+                const parsedMoney = JSON.parse(storedMoney);
+                return parsedMoney.money || 0; // Valor padrão se money não estiver definido
+            } catch (e) {
+                console.error("Failed to parse user from localStorage", e);
+                return 0; // Valor padrão em caso de erro
+            }
+        }
+        return 0; // Valor padrão se não houver item armazenado
+    })(),
 };
 
 const fatecoinSlice = createSlice({
-  name: 'fatecoin',
-  initialState,
-  reducers: {
-    setCoins(state, action: PayloadAction<number>) {
-      state.coins = action.payload;
+    name: 'fatecoin',
+    initialState,
+    reducers: {
+        setCoins(state, action: PayloadAction<number>) {
+            state.coins = action.payload;
+        },
     },
-  },
 });
 
 export const { setCoins } = fatecoinSlice.actions;
 export default fatecoinSlice.reducer;
 
-export const getCoins = (state: RootState) => state.fatecoin.coins
+export const getCoins = (state: RootState) => state.fatecoin.coins;
